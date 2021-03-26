1bd4f49cc1f871af4d0f80dd8dbdfc604e549186
Mfstat27 <tracey_thomas@live.com>
2021-03-25 21:02:17 -0400

    created server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+
+
+
+app.listen(PORT, ()=> {
+    console.log(`Listening on port: ${PORT}`)
+})
