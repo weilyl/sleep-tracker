f9762abb421d9b5beabe716f2d4049d64cb2fbf9
Weily Lang <weily.lang@gmail.com>
2021-03-28 18:15:30 -0400

    adds wakeup property that generates automatically

--- migrations/20210326020530_create_entries_table.js
+++ migrations/20210326020530_create_entries_table.js
@@ -3,7 +3,8 @@ exports.up = function(knex) {
   return knex.schema.createTable('entries', function(table) {
       table.increments('id');
       table.integer('hours').notNullable();
-      table.date('day');
+      table.date('day')      
+      table.timestamp('wakeup').defaultTo(knex.fn.now())      
       table.timestamps(true, true);
       table.text('description');
       table.string('title');
